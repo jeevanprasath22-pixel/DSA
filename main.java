b1f1c2febfd5bdd3d18e44ef4911d8a678070531
Jeevan prasath <jeevansuguna22@gmail.com>
2026-01-26 11:25:46 +0530

    refactor main method to enhance output and add comments

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+        System.out.println("This is Jeevan prasath New to VS code and github egger to learn");
+        //this is the changes made in code;
+        //this is the 2 change made in the code;
+        
     }
 }
